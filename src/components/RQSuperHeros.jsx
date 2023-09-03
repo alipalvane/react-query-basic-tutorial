d3d54a9c59f5ff761291de54b13d8c8488fa417d
Ali <alipalvane@gmail.com>
2023-09-03 23:52:41 +0330

    add caching and custom time

--- src/components/RQSuperHeros.jsx
+++ src/components/RQSuperHeros.jsx
@@ -1,10 +1,18 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 
+const fetchHeros = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 const RQSuperHeros = () => {
-  const { isLoading, data, isError, error } = useQuery("super-heroes", () => {
-    return axios.get("http://localhost:4000/superheroes");
-  });
+  const { isLoading, data, isError, error, isFetching } = useQuery(
+    "super-heroes",
+    fetchHeros,
+    { cacheTime: 2000 }
+  );
+  console.log("isloading: ", isLoading);
+  console.log("is fetching: ", isFetching);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
