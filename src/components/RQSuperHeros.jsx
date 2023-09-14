31d245c79b82c6d9f414bb8bef0c9ced37e3cfcd
Ali <alipalvane@gmail.com>
2023-09-14 14:33:59 +0330

    add util for axios

--- src/components/RQSuperHeros.jsx
+++ src/components/RQSuperHeros.jsx
@@ -59,3 +59,4 @@ const RQSuperHeros = () => {
 };
 
 export default RQSuperHeros;
+
--- src/hooks/useSuperHero.js
+++ src/hooks/useSuperHero.js
@@ -1,12 +1,18 @@
 import { useQuery, useMutation, useQueryClient } from "react-query";
-import axios from "axios";
+import { request } from "../utils/axios-utils";
 
 const fetchHeros = () => {
-  return axios.get("http://localhost:4000/superheroes");
+  //return axios.get("http://localhost:4000/superheroes");
+  return request({ url: "superheroes" });
 };
 
 const addSuperHero = (hero) => {
-  return axios.post("http://localhost:4000/superheroes", hero);
+  //return axios.post("http://localhost:4000/superheroes", hero);
+  return request({
+    url: "http://localhost:4000/superheroes",
+    method: "post",
+    data: hero,
+  });
 };
 
 export const useSuperHero = (onSuccess, onError) => {
